99653e30f7c7e601aca8f5c074b649712ad8c5c0
Highvedy/hrdv <89665648+hrdv10@users.noreply.github.com>
2024-01-11 23:03:02 +0530

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,113 +1,22 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
-
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const BlogPostTemplate = ({
-  data: { previous, next, site, markdownRemark: post },
-  location,
-}) => {
-  const siteTitle = site.siteMetadata?.title || `Title`
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <article
-        className="blog-post"
-        itemScope
-        itemType="http://schema.org/Article"
-      >
-        <header>
-          <h1 itemProp="headline">{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-        </header>
-        <section
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          itemProp="articleBody"
-        />
-        <hr />
-        <footer>
-          <Bio />
-        </footer>
-      </article>
-      <nav className="blog-post-nav">
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </Layout>
-  )
-}
-
-export const Head = ({ data: { markdownRemark: post } }) => {
-  return (
-    <Seo
-      title={post.frontmatter.title}
-      description={post.frontmatter.description || post.excerpt}
-    />
-  )
-}
-
-export default BlogPostTemplate
-
-export const pageQuery = graphql`
-  query BlogPostBySlug(
-    $id: String!
-    $previousPostId: String
-    $nextPostId: String
-  ) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    markdownRemark(id: { eq: $id }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-      }
-    }
-    previous: markdownRemark(id: { eq: $previousPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-    next: markdownRemark(id: { eq: $nextPostId }) {
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-  }
-`
+<Helmet
+htmlAttributes={{ lang: 'en' }}
+meta={[{ name: 'description', content: siteDescription }]}
+title={siteTitle}
+link={[{
+  href:"https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css",
+  rel:"stylesheet",
+  type:"text/css" 
+}]}
+script={[{ 
+  type: 'text/javascript', 
+  url:"",
+  id: "snipcart",
+  "data-api-key": "MGUyMWI4NjktZGZmYS00N2RlLThlNGQtZmU5YjZmNTBhNGMwNjM4NDA1ODg3MzI5MTQ4Mzg4",
+  src:"https://cdn.snipcart.com/scripts/2.0/snipcart.js" 
+},{
+  type: 'text/javascript',
+  src:"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"
+}]} />
+       
+          
+ 
